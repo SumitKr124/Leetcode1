05a3ff1f0afb8b55574bf3450c5b94a6606fd54c
sumit kumar <148712420+SumitKr124@users.noreply.github.com>
2026-07-22 18:05:11 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (14.01%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -5,32 +5,24 @@
  *     ListNode next;
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) {
- *         this.val = val;
- *         this.next = next;
- *     }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode reverseList(ListNode head) {
-
         ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-
-            // Store next node
-            ListNode next = curr.next;
-
-            // Reverse current node's pointer
-            curr.next = prev;
-
-            // Move prev and curr one step forward
-            prev = curr;
-            curr = next;
+        ListNode current = head;
+        while(current != null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+            
         }
-
         return prev;
+
+
+
+
     }
 }
